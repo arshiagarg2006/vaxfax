64dbb55cc27b7d8a80999309c7f179d8733c04ef
arshiagarg2006 <73620364+arshiagarg2006@users.noreply.github.com>
2022-03-20 00:28:48 -0400

    Update vax-requests.py

--- vax-requests.py
+++ vax-requests.py
@@ -1,5 +1,5 @@
 import csv
-from flask import Flask, render_template
+from flask import Flask, render_template, requests
 
 
 app = Flask(__name__)
@@ -9,68 +9,83 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/getInfo', methods= ['GET', 'POST'])
+def getInfo():
+  info = ""
+  state = requests.form('select1')
+  vaccine = requests.form('select2')
 
-state = "Columbia"
+  vaxfile = "static/vax.csv"
+  
+  fields = []
+  rows = []
+  
+  with open(vaxfile, 'r') as csvfile:
+      csvreader = csv.reader(csvfile, delimiter="\t")
+      
+      fields = next(csvreader)
+  
+      for row in csvreader:
+          rows.append(row)
+
+
+  for row in rows:
+    if (state in row[3]) & ("2020" in row[4]) & ("13-17" in row[6]):
+      if ((row[0] == "HPV") & ("Males and Females" in row[1])) or row[0] != "HPV":
+        if vaccine == "Meningococcal conjugate" & row[0] == "Meningococcal Conjugate":
+          info = info + vaccine + "vacinations with a " + row[1] + "have a " + row[7] + "coverage in your state."
+        elif vaccine == "Tdap" & row[0] == "Tdap":
+          info = info + vaccine + "vacinations with a " + row[1] + "have a " + row[7] + "coverage in your state."
+        elif vaccine == "Vericella":
+          info = info + vaccine + "vacinations with a " + row[1] + "have a " + row[7] + "coverage in your state."
+        elif vaccine == "HPV":
+          info = info + vaccine + "vacinations with a " + row[1] + "have a " + row[7] + "coverage in your state."
+        elif vaccine == "Hepatitis A":
+          info = info + vaccine + "vacinations with a " + row[1] + "have a " + row[7] + "coverage in your state."
+        elif vaccine == "MMR":
+          info = info + vaccine + "vacinations with a " + row[1] + "have a " + row[7] + "coverage in your state."
+        elif vaccine == "Hepatitis B":
+          info = info + vaccine + "vacinations with a " + row[1] + "have a " + row[7] + "coverage in your state."
+        # print(row[0] + " " + fields[1] + row[1] + " " + fields[7] + row[7])
+  
 
 
 
 
-vaxfile = "static/vax.csv"
- 
-fields = []
-rows = []
- 
-with open(vaxfile, 'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter="\t")
-     
-    fields = next(csvreader)
- 
-    for row in csvreader:
-        rows.append(row)
 
 
-for row in rows:
-  if (state in row[3]) & ("2020" in row[4]) & ("13-17" in row[6]):
-    if ((row[0] == "HPV") & ("Males and Females" in row[1])) or row[0] != "HPV":
-      print(row[0] + " " + fields[1] + row[1] + " " + fields[7] + row[7])
+  reqfile = "static/req.csv"
+  
+  fields1 = []
+  rows1 = []
 
-vaccine = "Meningococcal"
+  def requirement(num, state): 
+    for row in rows1:
+      if state in row[0]:
+        return row[num]
 
-reqfile = "static/req.csv"
- 
-fields1 = []
-rows1 = []
+  
+  with open(reqfile, 'r') as csvfile:
+      csvreader = csv.reader(csvfile, delimiter=",")
+      
+      fields1 = next(csvreader)
+  
+      for row in csvreader:
+          rows1.append(row)
 
-def requirement(num, state): 
-  for row in rows1:
-    if state in row[0]:
-      return row[num]
-
- 
-with open(reqfile, 'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-     
-    fields1 = next(csvreader)
- 
-    for row in csvreader:
-        rows1.append(row)
-
-if vaccine == "Meningococcal":
-  print(requirement(1, state))
-elif vaccine == "TDAP":
-  print(requirement(2, state))
-elif vaccine == "Vericella":
-  print(requirement(3, state))
-elif vaccine == "HPV":
-  print(requirement(4, state))
-elif vaccine == "HepA":
-  print(requirement(5, state))
-elif vaccine == "MMR":
-  print(requirement(6, state))
-elif vaccine == "HepB":
-  print(requirement(7, state))
+  if vaccine == "Meningococcal conjugate":
+    requirement(1, state)
+  elif vaccine == "Tdap":
+    print(requirement(2, state))
+  elif vaccine == "Vericella":
+    print(requirement(3, state))
+  elif vaccine == "HPV":
+    print(requirement(4, state))
+  elif vaccine == "Hepatitis A":
+    print(requirement(5, state))
+  elif vaccine == "MMR":
+    print(requirement(6, state))
+  elif vaccine == "Hepatitis B":
+    print(requirement(7, state))
 if __name__ == '__main__':
 	app.run()
-
-
-
